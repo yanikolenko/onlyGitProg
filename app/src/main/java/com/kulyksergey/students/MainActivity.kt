a3fdd50812548b88fab5738b1e34bfdc9b03c901
Sergey.Kulyk <sergey.kulyk.developer@gmail.com>
2021-09-23 16:14:42 +0300

    remove code generation

--- app/src/main/java/com/kulyksergey/students/MainActivity.kt
+++ app/src/main/java/com/kulyksergey/students/MainActivity.kt
@@ -9,18 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val str = StringBuilder()
-        repeat(26) {
-            str.append(
-            "        <TextView\n" +
-                    "            style=\"@style/StudentName\"\n" +
-                    "            android:layout_width=\"match_parent\"\n" +
-                    "            android:layout_height=\"wrap_content\"\n" +
-                    "            android:text=\"${it + 1}. \" />"
-            )
-            str.append("\n")
-        }
-        println(str.toString())
     }
 }
